be55b775371f5974c5c20cc3bd2d0bf7f8b26dff
bigdummyPro <93147630+bigdummyPro@users.noreply.github.com>
2021-12-14 10:40:58 -0600

    Update Icons.js

--- client/src/components/Icons.js
+++ client/src/components/Icons.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
 const Icons = ({setContent, content, theme}) => {
-    const reactions = [   
-        '❤️', '😆', '😯', '😢', '😡', '👍', '👎', '😄',
-        '😂', '😍', '😘', '😗', '😚', '😳', '😭', '😓',
-        '😤', '🤤', '👻', '💀', '🤐', '😴', '😷', '😵'
-    ]
+    var myArray = [
+      "How are you doing",
+      "What are we thinking today",
+      "What are we writing today",
+      "Whats on your mind",
+      "How are you feeling",
+      "How are you"
+    ];
+
+    var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
 
     return (
         <div className="nav-item dropdown" 
@@ -13,14 +18,14 @@ const Icons = ({setContent, content, theme}) => {
             
             <span className="nav-link position-relative px-1" id="navbarDropdown" 
             role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span style={{opacity: 0.4}}>😄</span>
+                <span style={{opacity: 0.4}}>Random Bible Verse</span>
             </span>
 
             <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <div className="reactions">
+                <div className="myArray">
                     {
-                        reactions.map(icon => (
-                            <span key={icon} onClick={() => setContent(content + icon)}>
+                        randomItem.map(icon => (
+                            <span key={randomItem} onClick={() => setContent(content + {randomItem})}>
                                 {icon}
                             </span>
                         ))
