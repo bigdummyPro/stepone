1e10ecd8b833e6c00beb15bf60834c2fb884d0f4
bigdummyPro <93147630+bigdummyPro@users.noreply.github.com>
2021-12-14 10:04:42 -0600

    Update Status.js
    Added a randomization feature for text displayed when you want to insert a message

--- client/src/components/home/Status.js
+++ client/src/components/home/Status.js
@@ -6,14 +6,19 @@ import { GLOBALTYPES } from '../../redux/actions/globalTypes'
 const Status = () => {
     const { auth } = useSelector(state => state)
     const dispatch = useDispatch()
-
+    var myArray = [
+      "Hey whats up ",
+      "Whats on your mind ",
+      "How are you feeling "
+    ];
+    var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
     return (
         <div className="status my-3 d-flex">
             <Avatar src={auth.user.avatar} size="big-avatar" />
             
             <button className="statusBtn flex-fill"
             onClick={() => dispatch({ type: GLOBALTYPES.STATUS, payload: true })}>
-                Whats on your mind?
+                {auth.user.username}
             </button>
         </div>
     )
