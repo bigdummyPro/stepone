293705b0cd78134fcbd9a682c09ab248bcdba029
bigdummyPro <93147630+bigdummyPro@users.noreply.github.com>
2022-01-23 16:49:50 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ app.use('/api/message', messageRouter);
 app.use('/api/stories', storiesRouter)
 
 
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 5000;
 
 //Define socket
 const http = require('http');
